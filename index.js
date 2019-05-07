c7b64df33a040bbad6d8839098ca80f70a333485
Le H Ngo <seiji@o2.pl>
2019-05-07 12:50:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 function add(a, b) {
   return a + b;
 }
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b
+}
+
+function inc(n) {
+  return n++
+}
+
+function dec(n) {
+  return n--
+}
+
